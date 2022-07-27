231967bab5ee2c03ae50daacc5e9d8c61d502b7e
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2022-07-27 11:47:31 -0700

    Basic Calculator

--- basicCalculator/js/scripts.js
+++ basicCalculator/js/scripts.js
@@ -1,9 +1,66 @@
-let firstUserEntry = Number(prompt('Enter your first number:'));
-let secondUserEntry = Number(prompt('Enter your second number:'));
+let firstUserEntry;
+let secondUserEntry;
+let calc;
+
+function gatherData() {
+let done = false;
+do {
+    
+    firstUserEntry = parseFloat(prompt('Enter your first number:'));
+    if  (isNaN(firstUserEntry) ) {
+        alert('Please enter numbers only.');
+        continue;
+        } 
+    secondUserEntry = parseFloat(prompt('Enter your second number:'));
+    if  (isNaN(secondUserEntry)) {
+        alert('Please enter numbers only.');
+        continue;
+        }    
+    
+     
+    calc = prompt('What math operation would you like to do with these two numbers? \nEnter:\n "+" for addition \n "-" for subtraction \n "*" for multiplication \n "/" for division');
+    if (calc !== '+' && calc !== '-' && calc !== '*' && calc !== '/') {
+        alert('Please enter a valid operator');
+    continue; }
+    if (calc == '/' && secondUserEntry == 0){
+        alert('You cannot divide a number by 0.');   
+        continue;
+    }  
+    done = true;
+    }while (!done);
 
 
+}
+
+gatherData();
 
 
 function basicCalculator() {
+   
+    switch (calc) {
+    case '+':
+        answer = firstUserEntry + secondUserEntry; 
+        alert(`the sum of your numbers is: ${answer}`);    
+            
+        break;
+    case '-':
+        answer = firstUserEntry - secondUserEntry;
+        alert(`the difference of your numbers is: ${answer}`);
+            
+        break;
+    case '*':
+        answer = firstUserEntry * secondUserEntry;
+        alert(`the product of your numbers is: ${answer}`);
+            
+        break;
+    case '/':
+        answer = firstUserEntry / secondUserEntry;
+        alert(`the quotient of your numbers is: ${answer}`);
+        break;
+        
+    default:
+        break;
+    }
 
 }
+basicCalculator();
